ebc7590e4fb2ddccd5fee33a1c4e10e22f510cc7
Luckydev0331 <jameswilliam03031@gmail.com>
2024-05-15 12:58:15 +0200

    update the fucntion to display the assign setting

--- controllers/client/getAllClientsController.js
+++ controllers/client/getAllClientsController.js
@@ -2,7 +2,6 @@ const Client = require("../../models/client");
 
 const getAllClients = async function (req, res) {
     try {
-        console.log("✅🌿✅")
         const clients = await Client.find();
         res.status(200).json(clients);
     } catch (err) {
--- controllers/client/getClientController.js
+++ controllers/client/getClientController.js
@@ -1,11 +1,16 @@
 const Client = require("../../models/client");
 
 const getClient = async function (req, res) {
+    console.log("here is getClient")
     try {
         const profileID = req.params.profileID;
         const client = await Client.findOne({Profile_ID : profileID});
-        console.log("✅👧✅", client)
-        res.status(200).json(client);
+        if(client) {
+            res.status(200).json(client);
+        } else {
+            res.error(404).json({message: "Not found"})
+            console.log("not found")
+        }
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
