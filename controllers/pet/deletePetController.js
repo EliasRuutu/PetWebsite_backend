88c873f439a997452a2cc33a0fb8a008bbfe96e4
Luckydev0331 <jameswilliam03031@gmail.com>
2024-05-16 13:46:32 +0200

    update the deletePetController

--- controllers/pet/deletePetController.js
+++ controllers/pet/deletePetController.js
@@ -4,14 +4,12 @@ const Assign = require("../../models/assign")
 const deletePet = async (req, res) => {
     const Profile_ID = req.params.id;
     const idTag = req.params.idTag;
+    console.log("Profile_ID", Profile_ID, "idTag", idTag);
     try {
-        Pet.findOneAndDelete({Profile_ID: Profile_ID}).then(
-            res.status(200).json({message:"successfully delete"})
-        ).catch(err => {
-            console.log(err)
-        })
+        await Pet.findOneAndDelete({Profile_ID: Profile_ID});
+        await Assign.findOneAndUpdate({Tag_ID: idTag}, {Assigned_Pet: null}, {new: true, upsert: false});
 
-        Assign.findOneAndUpdate({idTag : idTag}, {Assigned_Pet : null}, {new : true})
+        res.status(200).json({message: "successfully deleted"});
     } catch(error) {
 
     }
